ab7b2a2b9cf895717354ab0925ad94ec8413a423
kanglaeheon <kanglaeheon@gmail.com>
2021-09-15 13:38:23 +0900

    SFC

---
+++ sfc/src/components/CartFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>&copy; Bit Academy</p>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    p {
+        font-style: italic;
+        font-weight: bold;
+    }
+</style>
